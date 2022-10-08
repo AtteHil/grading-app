68513e6269a3e6c81ace919ecaeed6f216465d92
Rookit15 <attehiltunen@hotmail.com>
2022-10-08 20:02:07 +0300

    Update grade.py
    made end of grading open new window for results

--- grade.py
+++ grade.py
@@ -2,7 +2,7 @@ import os
 from tkinter import *
 
 from PIL import ImageTk, Image
-
+a_list=[]
 root = Tk()
 root.title('Grade this')
 root.geometry("500x600")
@@ -16,24 +16,47 @@ root.iconbitmap(path)
 my_image1= ImageTk.PhotoImage(Image.open("pics/longdrink.jpg").resize((500,500), Image.Resampling.LANCZOS))
 
 my_image2=ImageTk.PhotoImage(Image.open("pics/banana.jpg").resize((500,500), Image.Resampling.LANCZOS))
+my_image3=ImageTk.PhotoImage(Image.open("pics/karhu.jfif").resize((500,500), Image.Resampling.LANCZOS))
 
-list_images = [my_image1,my_image2]
+list_images = [my_image1,my_image2, my_image3]
+count= len(list_images)
 imgLabel= Label(image=my_image1)
 imgLabel.grid(row=1,columnspan=2)
+def makeTopLevel():
+    top = Toplevel()
+    top.geometry("500x600")
+    top.title('Result')
+    resultTitle= Label(top, text="Results are shown here").grid(row=0, columnspan=2)
+    
 
 def Good(number):
     global imgLabel
-    global button_good
-    global button_bad
-    imgLabel.grid_forget()
+    global count
+    if count == number-1:
+        makeTopLevel()
+        
+
+        
+   
     imgLabel= Label(image=list_images[number-1]).grid(row=1,columnspan=2)
     
+    Goodbutton= Button(root, text="Good", fg='black', bg='green',padx=10, pady=5 , command=lambda: Good(number+1)).grid(row=2,column=0)
+    badButton=Button(root, text="Bad", fg='white', bg='red', padx=10, pady=5,command=lambda: Bad(number+1)).grid(row=2,column=1)
+    a_list.append(1)
     
 def Bad(number):
-    global my_label
-    global button_good
-    global button_bad
-    root.bell()
+    global imgLabel
+    global count
+    if count == number-1:
+        makeTopLevel()
+
+    imgLabel= Label(image=list_images[number-1]).grid(row=1,columnspan=2)
+    
+    Goodbutton= Button(root, text="Good", fg='black', bg='green',padx=10, pady=5 , command=lambda: Good(number+1)).grid(row=2,column=0)
+    badButton=Button(root, text="Bad", fg='white', bg='red', padx=10, pady=5,command=lambda: Bad(number+1)).grid(row=2,column=1)
+    a_list.append(0)
+    
+
 
 
 Goodbutton= Button(root, text="Good", fg='black', bg='green',padx=10, pady=5 , command=lambda: Good(2)).grid(row=2,column=0)
