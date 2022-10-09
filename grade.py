ec3a8bb00da1f64012342c86edae00b0af2bbc26
Rookit15 <attehiltunen@hotmail.com>
2022-10-09 13:29:39 +0300

    First working version
    Working grading app(version 1)

--- grade.py
+++ grade.py
@@ -1,46 +1,58 @@
 import os
 from tkinter import *
-from turtle import left
-
 from PIL import ImageTk, Image
-a_list=[]
-root = Tk()
+
+
+a_list=[] ## set to global so I can access it in different methods
+root = Tk() ## root window set
 root.title('Grade this')
 root.geometry("500x600")
 
+
 def main():
-    SetupStart()
-
-
-def SetupStart():
-    global list_images
-    global small_images
-    global count
     
+    global count
+
     path= os.getcwd()
     root.iconbitmap(path)
     Descritpion= Label(root, text="Grade product show with buttons below").grid(row=0, columnspan=2)
-    my_image1= ImageTk.PhotoImage(Image.open("pics/longdrink.jpg").resize((500,500), Image.Resampling.LANCZOS))
-
-    my_image2=ImageTk.PhotoImage(Image.open("pics/banana.jpg").resize((500,500), Image.Resampling.LANCZOS))
-    my_image3=ImageTk.PhotoImage(Image.open("pics/karhu.jfif").resize((500,500), Image.Resampling.LANCZOS))
-    small_image2=ImageTk.PhotoImage(Image.open("pics/banana.jpg").resize((90,90), Image.Resampling.LANCZOS))
-    small_image1= ImageTk.PhotoImage(Image.open("pics/longdrink.jpg").resize((90,90), Image.Resampling.LANCZOS))
-    small_image3=ImageTk.PhotoImage(Image.open("pics/karhu.jfif").resize((90,90), Image.Resampling.LANCZOS))
-
-    list_images = [my_image1,my_image2, my_image3]
-    small_images= [small_image1, small_image2, small_image3]
+    makelists()
     count= len(list_images)
-    imgLabel= Label(image=my_image1)
+    imgLabel= Label(image=list_images[0])
     imgLabel.grid(row=1,columnspan=2)
         
     Goodbutton= Button(root, text="Good", fg='black', bg='green',padx=10, pady=5 , command=lambda: Good(2)).grid(row=2,column=0)
 
     badButton=Button(root, text="Bad", fg='white', bg='red', padx=10, pady=5,command=lambda: Bad(2)).grid(row=2,column=1)
 
+def makelists():
+    global list_images
+    global small_images
+    my_image1= ImageTk.PhotoImage(Image.open("pics/longdrink.jpg").resize((500,500), Image.Resampling.LANCZOS))
+
+    my_image2=ImageTk.PhotoImage(Image.open("pics/banana.jpg").resize((500,500), Image.Resampling.LANCZOS))
+    my_image3=ImageTk.PhotoImage(Image.open("pics/karhu.jfif").resize((500,500), Image.Resampling.LANCZOS))
+    my_image4=ImageTk.PhotoImage(Image.open("pics/breezer.png").resize((500,500), Image.Resampling.LANCZOS))
+    my_image5=ImageTk.PhotoImage(Image.open("pics/cider.jpg").resize((500,500), Image.Resampling.LANCZOS))
+    my_image6=ImageTk.PhotoImage(Image.open("pics/sandels.jpg").resize((500,500), Image.Resampling.LANCZOS))
+    my_image7=ImageTk.PhotoImage(Image.open("pics/longdrinkKoff.jpg").resize((500,500), Image.Resampling.LANCZOS))
+    list_images = [my_image1,my_image2, my_image3, my_image4,my_image5,my_image6,my_image7]
+
+    small_image2=ImageTk.PhotoImage(Image.open("pics/banana.jpg").resize((90,90), Image.Resampling.LANCZOS))
+    small_image1= ImageTk.PhotoImage(Image.open("pics/longdrink.jpg").resize((90,90), Image.Resampling.LANCZOS))
+    small_image3=ImageTk.PhotoImage(Image.open("pics/karhu.jfif").resize((90,90), Image.Resampling.LANCZOS))
+    small_image4=ImageTk.PhotoImage(Image.open("pics/breezer.png").resize((90,90), Image.Resampling.LANCZOS))
+    small_image5=ImageTk.PhotoImage(Image.open("pics/cider.jpg").resize((90,90), Image.Resampling.LANCZOS))
+    small_image6=ImageTk.PhotoImage(Image.open("pics/sandels.jpg").resize((90,90), Image.Resampling.LANCZOS))
+    small_image7=ImageTk.PhotoImage(Image.open("pics/longdrinkKoff.jpg").resize((90,90), Image.Resampling.LANCZOS))
+    
+    small_images= [small_image1, small_image2, small_image3, small_image4, small_image5, small_image6, small_image7]
 
 
-def makeTopLevel():
+
+
+
+def makeTopLevel(): ## opens new window and makes your chart
     top = Toplevel()
     top.geometry("500x460")
     top.title('Result')
@@ -55,7 +67,6 @@ def makeTopLevel():
         
         if a_list[i]==0:
             
-
             imgLabel= Label(top,image=small_images[i]).grid(row=c,column=a)
             
             if c==3:
@@ -64,6 +75,7 @@ def makeTopLevel():
                 c=3
             if c==3:
                 a+=1
+
         else:
             imgLabel = Label(top, image=small_images[i]).grid(row=d, column=b)
             
@@ -78,12 +90,13 @@ def makeTopLevel():
     
     
 
-def Good(number):
+def Good(number): ##Good button function
     global imgLabel
     
     if count == number-1:
         a_list.append(1)
         makeTopLevel()
+        return
    
     imgLabel= Label(image=list_images[number-1]).grid(row=1,columnspan=2)
     
@@ -91,12 +104,13 @@ def Good(number):
     badButton=Button(root, text="Bad", fg='white', bg='red', padx=10, pady=5,command=lambda: Bad(number+1)).grid(row=2,column=1)
     a_list.append(1)
     
-def Bad(number):
+def Bad(number): ##bad button function
     global imgLabel
     global count
     if count == number-1:
         a_list.append(0)
         makeTopLevel()
+        return
 
     imgLabel= Label(image=list_images[number-1]).grid(row=1,columnspan=2)
     
@@ -110,4 +124,3 @@ def Bad(number):
 
 main()
 root.mainloop()
-
